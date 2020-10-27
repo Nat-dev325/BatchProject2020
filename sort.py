02808889e121ec6c0a9077df11e305cb2642032f
Nat-dev325 <73475368+Nat-dev325@users.noreply.github.com>
2020-10-27 19:03:30 +0800

    Update sort.py
    Added bubblesort. Also suggest ko parang may format din sa pagprint ng new toys list. Kahit copy paste nalang to (or gawin new function if lahat gagamit nitong pangdisplay na function). Pero idk, baka icopy paste nalang natin or call function nung code ng command na [Print Toys]
    for i in range(0,len(toys)):
            for j in toys[i]:
                #'{0: <number}' is for format purposes
                if j == "Name":
                    display += str(j) + ": " + '{0: <15}'.format(str(toys[i][j]))
                    continue
                if j == "Feet":
                    display += str(j) + ": " + '{0: <6}'.format(str(toys[i][j]))
                    continue
                display += str(j) + ": " + '{0: <10}'.format(str(toys[i][j]))
            display += "\n"
        return display

--- sort.py
+++ sort.py
@@ -1,5 +1,28 @@
 # Library for sorting functions
 
+# Bubble Sort
+def BubbleSort(toys):
+    #Number of iterations; Ex. 5 elements = 5 iterations
+    for i in range(0,len(toys)):
+        #Indices; len(toys)-1 because there is no index that is the length of a list
+        for j in range(0,len(toys)-1):
+            if toys[j]["Name"] > toys[j+1]["Name"]:
+                toys[j], toys[j+1] = toys[j+1], toys[j]
+
+    display = ""
+    for i in range(0,len(toys)):
+        for j in toys[i]:
+            #'{0: <number}' is for format purposes
+            if j == "Name":
+                display += str(j) + ": " + '{0: <15}'.format(str(toys[i][j]))
+                continue
+            if j == "Feet":
+                display += str(j) + ": " + '{0: <6}'.format(str(toys[i][j]))
+                continue
+            display += str(j) + ": " + '{0: <10}'.format(str(toys[i][j]))
+        display += "\n"
+    return display
+
 # Insertion Sort
 def InsertionSort(toys):
 
